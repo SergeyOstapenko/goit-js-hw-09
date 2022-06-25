45f15bc07e1adec1fb0c9fb3b8a0a20bbafc9cac
SergeyOstapenko <98986750+SergeyOstapenko@users.noreply.github.com>
2022-06-25 19:21:54 +0000

    Deploying to gh-pages from @ SergeyOstapenko/goit-js-hw-09@9c57842eaa090b7a723e10e0f304cd8a6932fd5a 🚀

---
+++ 01-color-switcher.44bb934b.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("body"),t=document.querySelector("button[data-start"),d=document.querySelector("button[data-stop");let o=0;function r(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}d.disabled=!0,t.addEventListener("click",(()=>{e.style.backgroundColor=r(),o=setInterval((()=>{e.style.backgroundColor=r()}),1e3),t.disabled=!0,d.disabled=!1})),d.addEventListener("click",(()=>{clearInterval(o),d.disabled=!0,t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.44bb934b.js.map
