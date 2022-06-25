9c57842eaa090b7a723e10e0f304cd8a6932fd5a
SergeyOstapenko <sergiy.v.ostapenko@gmail.com>
2022-06-25 22:21:18 +0300

    task 1-3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const body = document.querySelector('body');
+const btnStart = document.querySelector('button[data-start');
+const btnStop = document.querySelector('button[data-stop');
+let intervalId = 0;
+btnStop.disabled = true;
+btnStart.addEventListener('click', () => {
+  body.style.backgroundColor = getRandomHexColor();
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
 
+  if ((btnStart.disabled = true)) {
+    btnStop.disabled = false;
+  }
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(intervalId);
+  if ((btnStop.disabled = true)) {
+    btnStart.disabled = false;
+  }
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
